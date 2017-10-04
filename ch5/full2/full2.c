9131797f6cf789ba9d82165e856af6feea19c2ca
Samad Moiz <samadmoizc8@gmail.com>
2017-10-04 12:07:12 +0000

    Done full1, full2, p-array

---
+++ ch5/full2/full2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+char first[100]; /* First name of person we are working with */
+char last[100]; /* His last name */
+
+/* First and last name of the person (computed) */
+char full[200];
+
+int main() 
+{
+    printf("Enter first name: ");
+    fgets(first, sizeof(first), stdin);
+    
+    // trim the last character
+    first[strlen(first)-1] = '\0';
+    
+    printf("Enter last name: ");
+    fgets(last, sizeof(last), stdin);
+    
+    // trim the last character
+    last[strlen(last)-1] = '\0';
+    
+    strcpy(full, first);
+    strcat(full, " ");
+    strcat(full, last);
+    
+    printf("The name is %s\n", full);
+    return 0;
+}
