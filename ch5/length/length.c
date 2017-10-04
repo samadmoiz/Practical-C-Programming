8381fdc22e666487f5d2ac5cce534df9c55e636b
Samad Moiz <samadmoizc8@gmail.com>
2017-10-04 12:06:51 +0000

    Updated length

--- ch5/length/length.c
+++ ch5/length/length.c
@@ -1,28 +1,13 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
-char first[100]; /* First name of person we are working with */
-char last[100]; /* His last name */
+char line[100];
 
-/* First and last name of the person (computed) */
-char full[200];
-
-int main() 
+int main()
 {
-    printf("Enter first name: ");
-    fgets(first, sizeof(first), stdin);
+    printf("Enter a line: ");
+    fgets(line, sizeof(line), stdin);
     
-    first[strlen(first)-1] = '\0';
-    
-    printf("Enter last name: ");
-    fgets(last, sizeof(last), stdin);
-    
-    last[strlen(last)-1] = '\0';
-    
-    strcpy(full, first);
-    strcat(full, " ");
-    strcat(full, last);
-    
-    printf("The name is %s\n", full);
+    printf("The length of the line is: %d\n", strlen(line));
     return 0;
 }
